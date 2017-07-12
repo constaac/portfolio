b62e00e2f1d238aebb9569da86aef17f2e9a01fe
constaac <aconstantine719@gmail.com>
2017-07-12 17:46:38 -0400

    Adds smooth scrolling to links to page sections. Adds beginnings of customized content

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,6 +7,28 @@ $(() => {
   setAPIOrigin(location, config)
 })
 
+$('a[href*="#"]').click(function (event) {
+  if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+    let target = $(this.hash)
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
+    if (target.length) {
+      event.preventDefault()
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000, function () {
+        const $target = $(target)
+        $target.focus()
+        if ($target.is(':focus')) {
+          return false
+        } else {
+          $target.attr('tabindex', '-1')
+          $target.focus()
+        }
+      })
+    }
+  }
+})
+
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
